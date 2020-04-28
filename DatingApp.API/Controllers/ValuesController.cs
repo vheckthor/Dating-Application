ee157b92c11deef09aeb8e9a357717948a47adbe
Dare Victor Adebayo <adebayovicktor@gmail.com>
2020-04-28 02:08:33 +0100

    begining of the dating application

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Properties.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public  IActionResult Get()
+        {
+
+            var success = _context.Values.ToList();
+
+            return Ok(success);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var result = _context.Values.FirstOrDefault(x=>x.Id==id);
+            return Ok(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Properties/Models/Value.cs
@@ -0,0 +1,9 @@
+using System;
+namespace DatingApp.API.Properties.Models
+{
+    public class Value
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
